c735138ade553dd3b1982ef0dc54d325c56110a2
Matthew-Bounds-Teacher <122050578+Matthew-Bounds-Teacher@users.noreply.github.com>
2025-08-26 08:22:02 -0500

    Create Math.java

---
+++ Math.java
@@ -0,0 +1,37 @@
+public class Math{
+  // Java Math API
+  // https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html
+  public static void main(String[] args){
+    // The absolute value method accepts a number and returns that value's distance from 0.
+    //int val = Math.abs(-3);
+    //double var = Math.abs(2.4);
+    
+    // The ceiling method accepts a number and returns the next largest whole number as a double 
+    //double var = Math.ceil(2.31);
+    
+    // The ceiling method accepts a number and returns the next smallest whole number as a double
+    //double var = Math.floor(2.31);
+    
+    // log
+    
+    // The max method accepts two numbers and returns the larger of the two
+    //int val = Math.max(3,10);
+    //double var = Math.max(2.31, -32.1);
+    
+    // The min method accepts two numbers and returns the smaller of the two
+    //int val = Math.min(3,10);
+    //double var = Math.min(2.31, -32.1);
+    
+    // The power method accepts two doubles and returns the first argument to the power of the second
+    //double var = Math.pow(2, 3);
+    
+    // The random method accepts no arguments and returns a random number between 0.0 - 1.0 
+    //double var = Math.random();
+    // if you wish to modify the range of random numbers, there are several different methods to shift
+    //double var = Math.random() + 5; // this shifts the range 5 units to the right 5.0 - 6.0
+    //double var = Math.random() * 5.0; // this expands the range by 5 units 0.0 - 5.0
+     
+
+  }
+
+}
